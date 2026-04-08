1552289644b8a1d44b278f23f0997cfeced4cad4
Dakota Brink <git@kota.is>
2026-04-07 22:34:33 -0400

    reorganize

---
+++ src/chain.rs
@@ -0,0 +1,61 @@
+use anchor_lang::AccountDeserialize;
+use anyhow::Result;
+use helius::types::{
+    GetProgramAccountsV2Config, RpcTransactionsConfig, TransactionSubscribeFilter,
+    TransactionSubscribeOptions,
+};
+use serde_json::Value;
+use solana_client::{rpc_config::UiAccountEncoding, rpc_response::UiAccountData};
+use std::sync::Arc;
+use tokio_stream::StreamExt;
+
+use crate::{App, UiEvent};
+
+pub async fn stream_chain(app: Arc<App>) -> Result<()> {
+    let mut pagination_key = fetch_memos(&app, None).await?;
+
+    let ws = app.helius.ws().unwrap();
+
+    let config = RpcTransactionsConfig {
+        filter: TransactionSubscribeFilter::standard(&memos::ID),
+        options: TransactionSubscribeOptions::default(),
+    };
+    let (mut stream, _unsub) = ws.transaction_subscribe(config).await?;
+    while let Some(_) = stream.next().await {
+        pagination_key = fetch_memos(&app, pagination_key).await?;
+    }
+
+    Ok(())
+}
+
+async fn fetch_memos(ctx: &App, pagination_key: Option<String>) -> Result<Option<String>> {
+    let response = ctx
+        .helius
+        .rpc()
+        .get_program_accounts_v2(
+            memos::ID.to_string(),
+            GetProgramAccountsV2Config {
+                encoding: Some(helius::types::Encoding::Base64Zstd),
+                pagination_key,
+                ..Default::default()
+            },
+        )
+        .await?;
+
+    for gpa in response.accounts {
+        let bytes = gpa
+            .account
+            .data
+            .as_array()
+            .and_then(|v| v.as_array())
+            .and_then(|v: &[Value; 2]| v[0].as_str())
+            .map(|v| UiAccountData::Binary(v.to_string(), UiAccountEncoding::Base64Zstd))
+            .and_then(|v| v.decode())
+            .expect("Data arrived in unexpected format.");
+
+        let memo = memos::Memo::try_deserialize(&mut &*bytes)?;
+        let _ = ctx.ui_tx.send(UiEvent::NewMemo((gpa.pubkey, memo)));
+    }
+
+    Ok(response.pagination_key)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
+mod chain;
+
 use anchor_client::{Client, Cluster, CommitmentConfig, Signer};
 use anchor_lang::prelude::*;
 use anyhow::Result;
-use base64::Engine;
-use base64::alphabet::{STANDARD, URL_SAFE};
-use base64::prelude::{BASE64_STANDARD, BASE64_STANDARD_NO_PAD, BASE64_URL_SAFE};
 use dotenvy::{dotenv, var};
-use helius::types::{
-    Cluster as HeliusCluster, CreateWebhookRequest, GetProgramAccountsV2Config, TransactionType,
-    WebhookType,
-};
+use helius::types::{Cluster as HeliusCluster, GetProgramAccountsV2Config};
 use helius::types::{
     RpcTransactionsConfig, TransactionSubscribeFilter, TransactionSubscribeOptions,
 };
@@ -18,116 +14,108 @@ use solana_client::rpc_config::UiAccountEncoding;
 use solana_client::rpc_response::UiAccountData;
 use solana_sdk::signature::{Keypair, read_keypair_file};
 use solana_system_interface::program as system_program;
-use solana_transaction_status::{
-    EncodedTransaction, UiInstruction, UiMessage, UiParsedInstruction,
-};
 use std::sync::Arc;
 use std::time::Duration;
+use tokio::runtime::Runtime;
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender, unbounded_channel};
 use tokio_stream::StreamExt;
+
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
 
 // #[derive(Deserialize)]
 // struct MemosIdl {
 // pub address: Pubkey,
 // }
 
-#[tokio::main]
-async fn main() -> Result<()> {
+enum UiEvent {
+    NewMemo((String, memos::Memo)),
+}
+
+struct App {
+    ui_rx: UnboundedReceiver<UiEvent>,
+    ui_tx: UnboundedSender<UiEvent>,
+    runtime: Runtime,
+    helius: Helius,
+    payer: Arc<Keypair>,
+}
+
+impl App {
+    fn new() -> Result<Self> {
+        let runtime = tokio::runtime::Builder::new_multi_thread().build()?;
+        let (ui_tx, ui_rx) = unbounded_channel();
+
+        let helius_key = var("HELIUS_KEY");
+        let helius = Helius::new(&helius_key?, HeliusCluster::Devnet)
+            .inspect_err(|e| {
+                if let HeliusError::Tungstenite(e) = e {
+                    if let tokio_tungstenite::tungstenite::Error::Http(e) = e {
+                        let body = e.body().as_ref().unwrap();
+                        println!("{}", String::from_utf8(body.clone()).unwrap());
+                    };
+                }
+            })
+            .unwrap();
+        let payer = read_keypair_file(&*shellexpand::tilde("~/.config/solana/id.json"))
+            .expect("Example requires a keypair file");
+        Ok(Self {
+            ui_rx,
+            ui_tx,
+            runtime,
+            helius,
+            payer: Arc::new(payer),
+        })
+    }
+
+    fn run(&self, terminal: &mut DefaultTerminal) -> Result<()> {
+        Ok(())
+    }
+}
+
+fn main() -> Result<()> {
     dotenv()?;
 
-    // let memo_idl_str = std::fs::read_to_string("./memos/target/idl/memos.json")
-    // .context("Unable to load memos idl")?;
-    // let memos_idl: MemosIdl = serde_json::from_str(&memo_idl_str)?;
+    let app = Arc::new(App::new()?);
+    app.runtime.spawn(chain::stream_chain(app.clone()));
 
-    let payer = read_keypair_file(&*shellexpand::tilde("~/.config/solana/id.json"))
-        .expect("Example requires a keypair file");
-    let cluster = Cluster::Devnet;
+    ratatui::run(move |terminal| app.run(terminal))
+}
 
-    let payer = Arc::new(payer);
+async fn publish_memo(app: Arc<App>, memo: String) -> Result<()> {
+    let memo_kp = Arc::new(Keypair::new());
+    let memo_pubkey = memo_kp.pubkey();
+
     let client = Client::new_with_options(
-        cluster.clone(),
-        payer.clone(),
+        Cluster::Devnet,
+        app.payer.clone(),
         CommitmentConfig::processed(),
     );
-
     let program = client.program(memos::ID)?;
 
-    tokio::task::spawn(async move {
-        loop {
-            let memo = Arc::new(Keypair::new());
-            let memo_pubkey = memo.pubkey();
-
-            program
-                .request()
-                .signer(memo)
-                .accounts(memos::accounts::StoreMemo {
-                    memo: memo_pubkey,
-                    signer: program.payer(),
-                    system_program: system_program::ID,
-                })
-                .args(memos::instruction::StoreMemo {
-                    text: "Hello there".to_string(),
-                })
-                .send()
-                .await
-                .unwrap();
-
-            println!("published");
-
-            tokio::time::sleep(Duration::from_secs(1)).await;
-        }
-    });
-
-    let helius_key = var("HELIUS_KEY");
-    println!("Helius key: {helius_key:?}");
-
-    let helius = Helius::new_async(&helius_key?, HeliusCluster::Devnet)
-        .await
-        .inspect_err(|e| {
-            if let HeliusError::Tungstenite(e) = e {
-                if let tokio_tungstenite::tungstenite::Error::Http(e) = e {
-                    let body = e.body().as_ref().unwrap();
-                    println!("{}", String::from_utf8(body.clone()).unwrap());
-                };
-            }
+    program
+        .request()
+        .signer(memo_kp)
+        .accounts(memos::accounts::StoreMemo {
+            memo: memo_pubkey,
+            signer: program.payer(),
+            system_program: system_program::ID,
         })
+        .args(memos::instruction::StoreMemo { text: memo })
+        .send()
+        .await
         .unwrap();
 
-    let response = helius
-        .rpc()
-        .get_program_accounts_v2(
-            memos::ID.to_string(),
-            GetProgramAccountsV2Config {
-                encoding: Some(helius::types::Encoding::Base64Zstd),
-                ..Default::default()
-            },
-        )
-        .await?;
-
-    for gpa in response.accounts {
-        let bytes = gpa
-            .account
-            .data
-            .as_array()
-            .and_then(|v| v.as_array())
-            .and_then(|v: &[Value; 2]| v[0].as_str())
-            .map(|v| UiAccountData::Binary(v.to_string(), UiAccountEncoding::Base64Zstd))
-            .and_then(|v| v.decode())
-            .expect("Data arrived in unexpected format.");
-
-        let memo = memos::Memo::try_deserialize(&mut &*bytes).unwrap();
-        println!("{}: {}", gpa.pubkey, memo.memo);
-    }
-
-    let ws = helius.ws().unwrap();
-
-    let config = RpcTransactionsConfig {
-        filter: TransactionSubscribeFilter::standard(&memos::ID),
-        options: TransactionSubscribeOptions::default(),
-    };
-    let (mut stream, _unsub) = ws.transaction_subscribe(config).await?;
-    while let Some(event) = stream.next().await {
-        println!("streamed {event:?}");
-    }
+    println!("published");
 
     Ok(())
 }
